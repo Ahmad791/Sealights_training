2642f7e0100cae40da7cca607729e2e49aec36e4
Ahmad linux <Ahmad.Wahbi.98@Gmail.com>
2023-07-05 12:16:43 +0300

    installed chrome driver because github

--- conftest.py
+++ conftest.py
@@ -3,9 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as firefoxService
 from webdriver_manager.firefox import GeckoDriverManager as firefoxDriverManager
 
+from selenium.webdriver.chrome.service import Service as chromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture
 def driver():
-    d = webdriver.Firefox(service=firefoxService(firefoxDriverManager().install()))# always install latest drivers
+    try:
+        d = webdriver.Firefox(service=firefoxService(firefoxDriverManager().install()))# always install latest drivers
+    except:
+        d=webdriver.Chrome(service=chromeService(ChromeDriverManager().install()))
     yield d
     d.quit()
